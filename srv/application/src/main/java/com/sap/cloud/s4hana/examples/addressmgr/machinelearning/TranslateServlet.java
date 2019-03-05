9e70d681b05c20592cd9ee1cca2f10055fc58971
Ekaterina Kruse <gavrilova.ev@gmail.com>
2019-03-05 18:37:31 +0000

    Add ML tasks

--- srv/application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/machinelearning/TranslateServlet.java
+++ srv/application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/machinelearning/TranslateServlet.java
@@ -1,63 +1,159 @@
 package com.sap.cloud.s4hana.examples.addressmgr.machinelearning;
 
-import org.apache.commons.lang3.StringUtils;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.sap.cloud.s4hana.examples.addressmgr.util.HttpServlet;
+import com.sap.cloud.sdk.cloudplatform.connectivity.Destination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpClientAccessor;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpEntityUtil;
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.slf4j.Logger;
-import rx.Observable;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import com.sap.cloud.s4hana.examples.addressmgr.machinelearning.commands.MlLanguageDetectionCommand;
-import com.sap.cloud.s4hana.examples.addressmgr.machinelearning.commands.MlTranslationCommand;
-import com.sap.cloud.s4hana.examples.addressmgr.util.HttpServlet;
-import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
 
 @WebServlet("/api/translate")
-public class TranslateServlet extends HttpServlet {
+public class TranslateServlet extends HttpServlet
+{
     private static final long serialVersionUID = 1L;
     private static final Logger logger = CloudLoggerFactory.getLogger(TranslateServlet.class);
+    private static final String TRANSLATION_PATH = "translation/translation";
 
     @Override
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
-            throws ServletException, IOException {
-        String input = request.getParameter("input");
-
-        response.setContentType("text/utf-8");
-        final PrintWriter responseWriter = response.getWriter();
+    protected void doGet( final HttpServletRequest request, final HttpServletResponse response )
+            throws ServletException, IOException
+    {
 
         try {
-            String output = translate(new MlService(), input, "en");
-            responseWriter.write("Translation: " + output);
+            String targetLang = "en";
+            String sourceLang = "de";
+            String source = request.getParameter("input");
+
+            String requestJson = createRequestJson(sourceLang, targetLang, source);
+            
+            // TODO: Prepare HttpPost query to translate "input"
+            // TODO: 1. Get the Destination object from the SCP destination configuration using the S/4HANA Cloud SDK
+            // TODO: 2. Using the Destination object construct the API endpoint for API sandbox by combining info from destination and TRANSLATION_PATH
+            // TODO: 3. Set additional postRequest headers: "Content-Type", "application/json" and "Accept", "application/json;charset=UTF-8"
+            // TODO: 4. Using the Destination object retrieve APIKey and add it to the postReqwuest header
+            
+            // Add http body
+            HttpEntity body = new StringEntity(requestJson, ContentType.APPLICATION_JSON);
+            postRequest.setEntity(body);
+
+            try {
+                // Getting cached http client for base URL, reuse of connection - and send request
+                final HttpResponse mlResponse = HttpClientAccessor.getHttpClient(mlDestination).execute(postRequest);
+                if (HttpStatus.SC_OK != mlResponse.getStatusLine().getStatusCode()) {
+                    throw new Exception("Request failed: " + mlResponse.getStatusLine());
+                }
+
+                // retrieve entity content (requested json with Accept header, so should be text) and close request
+                final String responseBody = HttpEntityUtil.getResponseBody(mlResponse);
+                final Map responseMap = new Gson().fromJson(responseBody, Map.class);
+                logger.debug("Response: {}", responseMap);
+                String translation = parseResponse(responseBody);
+                response.getWriter().write("Translation: " + translation);
+            } finally {
+                postRequest.releaseConnection();
+            }
+
         } catch (Exception e) {
-            logger.error("Problem in translation service access", e);
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            logger.error("Failure: " + e.getMessage(), e);
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Error: " + e.getMessage());
+        }
+
+    }
+
+    static String parseResponse(String response) {
+        Gson gson = new Gson();
+        TranslationResult translationResult = gson.fromJson(response, TranslationResult.class);
+
+        // Taking the first translation as a particular use case for the address manager
+        if (!translationResult.getUnits().isEmpty()) {
+            TranslationUnit translationUnit = translationResult.getUnits().get(0);
+            return translationUnit.getTranslations().get(0).getValue();
+
+        } else {
+            logger.error("No translations found");
+            throw new IllegalStateException("No tranlations found");
         }
     }
 
-    private String translate(MlService mlService, final String input, final String targetLang) throws Exception {
-        Observable<String> result = new MlLanguageDetectionCommand(mlService, input).observe().flatMap(
-                mlLanguageDetectionResult -> {
-                    final String sourceLang = mlLanguageDetectionResult.getLangCode();
-                    if (StringUtils.isBlank(sourceLang)) {
-                        logger.debug("Could not translate because language could not be determined: '{}'", input);
-                        return Observable.just("");
-                    }
+    private class TranslationResult {
+        List<TranslationUnit> units = Lists.newArrayList();
 
-                    return new MlTranslationCommand(sourceLang, targetLang, Collections.singletonList(input))
-                            .observe()
-                            .map(strings -> {
-                                if (strings.isEmpty()) {
-                                    logger.error("No results from translation 1's input - should not happen");
-                                    return "";
-                                }
-                                return strings.get(0);
-                            });
-                });
-        return result.toBlocking().single();
+        private List<TranslationUnit> getUnits() {
+            return units;
+        }
+
+        private void setUnits(final List<TranslationUnit> units) {
+            this.units = units;
+        }
+    }
+
+    private class TranslationUnit {
+        String value;
+        List<Translation> translations = Lists.newArrayList();
+
+        private String getValue() {
+            return value;
+        }
+        private void setValue(final String value) {
+            this.value = value;
+        }
+        private List<Translation> getTranslations() {
+            return translations;
+        }
+        private void setTranslations(final List<Translation> translations) {
+            this.translations = translations;
+        }
+    }
+
+    private class Translation {
+        String language;
+        String value;
+
+        private String getLanguage() {
+            return language;
+        }
+        private void setLanguage(final String language) {
+            this.language = language;
+        }
+        private String getValue() {
+            return value;
+        }
+        private void setValue(final String value) {
+            this.value = value;
+        }
+    }
+
+    static String createRequestJson( String sourceLanguage, String targetLanguage, String source )
+    {
+        final JsonObject requestContent = new JsonObject();
+        requestContent.addProperty("sourceLanguage", sourceLanguage);
+        requestContent.add("targetLanguages", new Gson().toJsonTree(new String[]{targetLanguage}));
+        requestContent.add("units", new Gson().toJsonTree(new Object[]{Collections.singletonMap("value", source)}));
+
+        return new Gson().toJson(requestContent);
     }
 }
